997227418f77d4a09bf9c1682f3d01aa2466215b
Jan Altensen <info@stricted.net>
2020-09-16 04:27:03 +0200

    ginna: libshims: add shim for android_memset32
     * memset32.S and android_memset.S taken from Q

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,11 @@ function blob_fixup() {
         patchelf --add-needed libqsap_shim.so "${2}"
         ;;
 
+    # memset shim
+    vendor/bin/charge_only_mode)
+        patchelf --add-needed libmemset_shim.so "${2}"
+        ;;
+
     # Fix xml version
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
