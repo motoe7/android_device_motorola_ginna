ffd5efd3f652051bfb6e9059c4b1bee3e9f58e40
Aaron Kling <webgeek1234@gmail.com>
2020-10-10 10:44:57 -0500

    ginna: Enable userspace lmkd
    Change-Id: I80d8786d5f93d5ba101c6bc0e652f6a3f14d1e34

--- properties.mk
+++ properties.mk
@@ -113,6 +113,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.fm.use_audio_session=true
 
 # LowMem
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.madvise-random=true
 
