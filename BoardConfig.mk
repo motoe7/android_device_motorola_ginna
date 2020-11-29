eda7bac5177e4a2696f3723656cfde4b62609b97
electimon <electimon@gmail.com>
2020-11-30 00:15:49 +0100

    ginna: Go enforcing!
    Change-Id: I81b47dee5ce1ec1f3e338482b7946c66453876c7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,7 +170,7 @@ TARGET_FS_CONFIG_GEN += \
 
 # Kernel
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 androidboot.usbconfigfs=true loop.max_part=7 printk.devkmsg=on androidboot.hab.csv=0 androidboot.hab.product=gin androidboot.hab.cid=0 androidboot.boot_devices=soc/7824900.sdhci
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_KERNEL_OFFSET := 0x00008000
