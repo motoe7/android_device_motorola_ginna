dbac824b93cf4ef49875ef4966cde110ac4624a6
electimon <electimon@gmail.com>
2021-01-02 20:29:21 +0800

    ginna: treewide: Add support for FTS touch

--- rootdir/bin/init.mmi.touch.sh
+++ rootdir/bin/init.mmi.touch.sh
@@ -1,7 +1,9 @@
-#!/system/bin/sh
+#!/vendor/bin/sh
 
-touch_path=/sys/chipone-tddi
-firmware_file=/vendor/firmware/chipone-tianma-ICNL9911S-0125-0000-ginna.bin
+touch_path_chipone=/sys/chipone-tddi
+touch_path_fts=/sys/devices/platform/soc/7af7000.spi/spi_master/spi7/spi7.0
+firmware_file_chipone=/vendor/firmware/chipone-tianma-ICNL9911S-0125-0000-ginna.bin
+firmware_file_fts=focaltech-hlt-ft8006s_aa-0b-0000-ginna.bin
 
 wait_for_poweron()
 {
@@ -10,8 +12,13 @@ wait_for_poweron()
 	local count
 	wait_nomore=60
 	count=0
+	if [[ -d $touch_path_chipone ]];
+	then
+	    readiness=$(cat $touch_path_chipone/poweron)
+	else
+	    readiness=$(cat $touch_path_fts/poweron)
+	fi
 	while true; do
-		readiness=$(cat $touch_path/poweron)
 		if [ "$readiness" == "1" ]; then
 			break;
 		fi
@@ -27,7 +34,12 @@ wait_for_poweron()
 
 wait_for_poweron
 
-echo 1 > $touch_path/forcereflash
-echo $firmware_file > $touch_path/doreflash
-
+if [[ -d $touch_path_chipone ]];
+then
+     echo 1 > $touch_path_chipone/forcereflash
+     echo $firmware_file_chipone > $touch_path/doreflash
+else
+     echo 1 > $touch_path_fts/forcereflash
+     echo $firmware_file_fts > $touch_path_fts/doreflash
+fi
 return 0
