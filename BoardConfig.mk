55bf3b9af2b1d5026f3b19fc39a75277b80abf57
PizzaG <pizzag98@gmail.com>
2021-01-27 22:32:22 +0800

    ginna: Rename sprint_apns.xml & Add Carriers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,7 +237,7 @@ TARGET_USES_INTERACTION_BOOST := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_OLD_MNC_FORMAT := true
 ENABLE_VENDOR_RIL_SERVICE := true
-CUSTOM_APNS_FILE := $(DEVICE_PATH)/configs/sprint_apns.xml
+CUSTOM_APNS_FILE := $(DEVICE_PATH)/configs/custom_apns.xml
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
