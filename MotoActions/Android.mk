d42d541777f89a5c921e7ec7930fd5c7ca804031
electimon <electimon@gmail.com>
2021-06-09 00:54:33 +0800

    MotoActions: Backport commits from lineage-18.1
    Signed-off-by: electimon <electimon@gmail.com>

--- MotoActions/Android.mk
+++ MotoActions/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := MotoActions
+LOCAL_PACKAGE_NAME := MotoActionsG
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_USE_AAPT2 := true
